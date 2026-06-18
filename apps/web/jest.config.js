7edce4d4bc71099c10136f6e6f1b6e0aa6c7b45f
Kubojah-Dan <danielkubojah04@gmail.com>
2026-06-18 11:21:31 +0530

    build: exclude tests from tsc build and ignore build dirs in Jest

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/.turbo/'],
   moduleNameMapper: {
     // Handle module aliases (if you have them in tsconfig.json)
     '^@/(.*)$': '<rootDir>/$1',
